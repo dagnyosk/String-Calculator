eb9663c09c6971e5c23e8623e7c0a8aed235717e
dagnyosk <dagnyoskg@gmail.com>
2017-10-16 00:21:21 +0000

    Add files via upload

---
+++ bin/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac src/test/resources/junit-4.12.jar -d classes src/test/java/is/ru/stingcalculator/calculator.java
---
+++ bin/compile_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp "src/test/resources/junit-4.12.jar:/classes" src/test/java/is/ru/stringcalculator/*.java -d classes
