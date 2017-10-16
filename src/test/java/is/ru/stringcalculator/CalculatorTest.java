d0390a81df6fd9db5926d1cee7ffcb23a6eca2d9
dagnyosk <dagnyoskg@gmail.com>
2017-10-16 00:24:39 +0000

    Add files via upload

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,39 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest{
+	@Test
+	public void testEmptyString(){
+		assertEquals(0, Calculator.add(""));
+	}
+	
+	@Test
+	public void testOneNumber(){
+		assertEquals(1, Calculator.add("1"));
+	}
+	
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
+	
+	@Test
+	public void testMultipleNumbers(){
+		assertEquals(6, Calculator.add("1,2,3"));
+	}
+	@Test
+	public void testTwoLinesMultipleNumbers(){
+		assertEquals(6, Calculator.add("1n2,3"));
+	}
+	@Test
+	public void testNumbersBigger(){
+		assertEquals(2, Calculator.add("1001,2"));
+	}
+	@Test
+	public void testThrowExceptionsNegativeMultiple(){
+		Calculator.add("2,-4,3,-5");
+	}
+
+}
