e45ca257aad4dc00cbbc41478279033b0a6bc8e8
dagnyosk <dagnyoskg@gmail.com>
2017-10-16 21:31:56 +0000

    Add files via upload

--- bin/compile_tests.sh
+++ bin/compile_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac -cp "src/test/resources/junit-4.12.jar:/classes" src/test/java/is/ru/stringcalculator/*.java -d classes
+javac -cp "src/test/resources/junit-4.12.jar:src/test/resources/hamcrest-core-1.3.jar:/classes" src/test/java/is/ru/stringcalculator/*.java -d classes
